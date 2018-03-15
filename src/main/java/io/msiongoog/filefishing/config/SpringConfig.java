569a5513ab694d460e981294f0df98ee88d63b6c
Shyam Raman <24shyam7@gmail.com>
2018-03-14 23:25:53 -0400

    added datasource configurations

--- src/main/java/io/msiongoog/filefishing/config/SpringConfig.java
+++ src/main/java/io/msiongoog/filefishing/config/SpringConfig.java
@@ -1,10 +1,16 @@
 package io.msiongoog.filefishing.config;
 
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @ComponentScan("io.msiongoog.filefishing")
 public class SpringConfig {
+	
+	@Autowired
+	DataSource dataSource;
 
 }
